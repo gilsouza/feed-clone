ce9f7de9117162f344854b7c7e2d00c39908c207
gilsouza <gilmar.costa@amedigital.com>
2020-11-08 18:41:44 -0300

    chore: Retira registro do ServiceWorker quando roda local

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Feed-Clone
 
 Projeto ReactJS para reproduzir a funcionalidade do feed de posts do twitter
+
 O APP também é um exemplo de PWA com algumas estratégias de cache em seu service-worker e pode ser adicionado à sua tela inicial =)
-Obs: Não houve otimização do PWA para iOS e etc.
+
+***Para executar localmente o APP como PWA é necessário editar o arquivo ./src/serviceWorker.js. Caso contrário um log com warning será exibido no console.***
+
+*Obs: Não houve otimização do PWA para iOS e etc.*
 
 ## Para acessar o projeto
 
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,21 @@
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+        // [::1] is the IPv6 localhost address.
+        window.location.hostname === '[::1]' ||
+        // 127.0.0.0/8 are considered localhost for IPv4.
+        window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        )
+);
+
 export const register = () => {
+    if (isLocalhost) {
+        console.warn(
+            '[Feed-Clone] O projeto está sendo executado em localhost. Deseja mesmo registar o serviceWorker?'
+        );
+        return;
+    }
+
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function () {
             navigator.serviceWorker.register('/sw.js');
