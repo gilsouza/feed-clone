cca772d8c0da8320f5f83636d6092bff274299a8
gilsouza <gilmar.costa@amedigital.com>
2020-11-08 16:39:36 -0300

    feat: Adiciona serviceworker

--- public/sw.js
+++ public/sw.js
@@ -1,39 +1,94 @@
-var CACHE_NAME = 'feed-clone';
+//This is the service worker with the Advanced caching
 
-var urlsToCache = [
-    '/',
-    '/static/js/bundle.js',
-    '/static/js/main.chunk.js',
-    '/static/js/1.chunk.js',
-    '/static/js/0.chunk.js',
-    '/favicon.ico',
-    '/css?family=Open+Sans',
-    '/icon?family=Material+Icons',
-];
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
+);
 
-self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+const HTML_CACHE = 'html';
+const JS_CACHE = 'javascript';
+const STYLE_CACHE = 'stylesheets';
+const IMAGE_CACHE = 'images';
+const FONT_CACHE = 'fonts';
+const AJAX_CACHE = 'fetch';
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            } else {
-                return fetch(event.request).then(function (res) {
-                    return caches.open(CACHE_NAME).then(function (cache) {
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                    });
-                });
-            }
-        })
-    );
-});
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'document',
+    new workbox.strategies.NetworkFirst({
+        cacheName: HTML_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 10,
+                maxAgeSeconds: 1 * 24 * 60 * 60, // 1 Day,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'script',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: JS_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'style',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: STYLE_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+                maxAgeSeconds: 1 * 24 * 60 * 60, // 1 Day,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'image',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: IMAGE_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.destination === 'font',
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: FONT_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+            }),
+        ],
+    })
+);
+
+workbox.routing.registerRoute(
+    /.*mock/,
+    new workbox.strategies.NetworkFirst({
+        cacheName: AJAX_CACHE,
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 15,
+                maxAgeSeconds: 1 * 24 * 60 * 60, // 1 Day,
+            }),
+        ],
+    })
+);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        for (let registration of registrations) {
-            registration.unregister();
-        }
-    });
-}
+serviceWorker.register();
