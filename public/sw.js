39f4c1f41ac56b714652858f5f01d72a4c250575
gilsouza <gilmar.costa@amedigital.com>
2020-11-08 01:55:02 -0300

    feat: Inicia PWA

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+var CACHE_NAME = 'feed-clone';
+
+var urlsToCache = [
+    '/',
+    '/static/js/bundle.js',
+    '/static/js/main.chunk.js',
+    '/static/js/1.chunk.js',
+    '/static/js/0.chunk.js',
+    '/favicon.ico',
+    '/css?family=Open+Sans',
+    '/icon?family=Material+Icons',
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request).then(function (res) {
+                    return caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request.url, res.clone());
+                        return res;
+                    });
+                });
+            }
+        })
+    );
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+serviceWorker.registerServiceWorker();
